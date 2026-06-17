5cb341cfa3f62a408a6f9e501954224ada9500c9
Ruona <r.okungbowa@corecomtechacademy.co.uk>
2026-06-17 16:16:22 +0100

    Fixed server.js

--- API/server.js
+++ API/server.js
@@ -41,56 +41,14 @@ app.post("/api/addtocart", addToCart());
 
 app.delete("/api/addtocart", removeFromCart());
 
+app.post("/api/bootstrap", bootstrap);
+
 app.post("/api/user");
 
 app.post("/api/login", loginUser());
 
 app.delete("/api/products", (request, response) => {
   runHandler(deleteProductHandler, request, response);
-});
-
-app.get("/api/addtocart", getCart);
-app.post("/api/addtocart", addToCart);
-app.delete("/api/addtocart", removeFromCart);
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
-import express from "express";
-import cors from "cors";
-
-import { postUsersHandler } from "../users.js";
-import { getImageUploadUrlHandler } from "../utility-functions.js";
-
-// Creating express app
-const app = express();
-// Port number
-const PORT = process.env.PORT || 3000;
-
-app.use(express.json());
-// Enabling CORS
-app.use(cors());
-
-// Sample api route for testing
-app.get("/", (request, response) => {
-  response.json("Welcome to our server");
-});
-
-app.post("/api/users", (req, res) => {
-  postUsersHandler(req, res);
-});
-
-app.post("/api/image-upload-url", (req, res) => {
-  getImageUploadUrlHandler(req, res);
-});
-
-app.post("/api/users", (req, res) => {
-  postUsersHandler(req, res);
-});
-
-app.post("/api/image-upload-url", (req, res) => {
-  getImageUploadUrlHandler(req, res);
 });
 
 app.listen(PORT, () => {
