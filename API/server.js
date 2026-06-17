37f65d0b6eb42e80068ed932528b1d301e259e3f
EtaYozhik <carter.singh@hotmail.com>
2026-06-17 13:42:50 +0100

    Added API routes for postUserHandler and getImageUploaderHandler

---
+++ API/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+import cors from "cors";
+
+import {postUsersHandler} from "../users.js";
+import {getImageUploadUrlHandler} from "../utility-functions.js";
+
+// Creating express app
+const app = express();
+// Port number
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+// Enabling CORS
+app.use(cors());
+
+// Sample api route for testing
+app.get("/", (request, response) => {
+  response.json("Welcome to our server");
+});
+
+app.post("/api/users", (req, res) => {
+  postUsersHandler(req, res);
+});
+
+app.post("/api/images/upload-url", (req, res) => {
+  getImageUploadUrlHandler(req, res);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
